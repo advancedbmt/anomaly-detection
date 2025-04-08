6971cfb700a4f790d3f7a32098e7116d4e1da7f4
Tomas Zuraev <tmaszuraev@gmail.com>
2025-04-07 20:54:00 -0400

    Add initial config.json for device schedule

---
+++ func.py
@@ -0,0 +1,31 @@
+import json
+from datetime import datetime
+
+
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+
+batch_data = config["state_timeline"]["batch"]
+
+
+def parse_entry(entry):
+    time_part, state_part = entry.split(" ", 1)
+    
+
+    if ":" in time_part:
+        time_obj = datetime.strptime(time_part.lower(), "%I:%M%p").time()
+    else:
+        time_obj = datetime.strptime(time_part.lower(), "%I%p").time()
+    
+    time_str = time_obj.strftime("%H:%M")
+    state_clean = state_part.replace("\\", " ")
+    
+    return (time_str, state_clean)
+
+
+parsed_batch = [parse_entry(entry) for entry in batch_data]
+
+
+print("Parsed batch schedule:")
+print(parsed_batch)
